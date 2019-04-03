fdeade60287c10e4974732283ea1737f2873d99b
jhchia7 <jhchia7@gmail.com>
2019-04-04 00:29:12 +0800

    remove unused routes

--- app.js
+++ app.js
@@ -219,8 +219,7 @@ var product2Router = require('./routes/product2');
 var singleRouter = require('./routes/single');
 var single2Router = require('./routes/single2');
 var termsRouter = require('./routes/terms');
-var signInRouter = require('./routes/signin');
-var signUpRouter = require('./routes/signup');
+var itemsRouter = require('./routes/items');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -246,8 +245,6 @@ app.use('/product2', product2Router);
 app.use('/single', singleRouter);
 app.use('/single2', single2Router);
 app.use('/terms', termsRouter);
-app.use('/signin', signInRouter);
-app.use('/signup', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
