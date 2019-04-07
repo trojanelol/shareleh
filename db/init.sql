661015c89468dc566c205efc77425b67bf746cab
Sheikh Umar <sheikh.umar.banafe@gmail.com>
2019-04-07 13:53:48 +0800

    Removal of attributes from rounds and items tables to items_rounds table.

--- db/init.sql
+++ db/init.sql
@@ -1,17 +1,17 @@
-
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS admins CASCADE;
-DROP TABLE IF EXISTS items CASCADE;
-DROP TABLE IF EXISTS rounds CASCADE;
-DROP TABLE IF EXISTS bids CASCADE;
-DROP TABLE IF EXISTS user_following CASCADE;
-DROP TABLE IF EXISTS item_review CASCADE;
-DROP TABLE IF EXISTS borrower_review CASCADE;
-DROP TABLE IF EXISTS lender_review CASCADE;
-DROP TABLE IF EXISTS wishlist CASCADE;
-DROP TABLE IF EXISTS item_categories CASCADE;
 DROP TABLE IF EXISTS tasks CASCADE;
 DROP TABLE IF EXISTS users_tasks CASCADE;
+DROP TABLE IF EXISTS rounds CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS user_following CASCADE;
+DROP TABLE IF EXISTS lender_review CASCADE;
+DROP TABLE IF EXISTS borrower_review CASCADE;
+DROP TABLE IF EXISTS item_categories CASCADE;
+DROP TABLE IF EXISTS wishlist CASCADE;
+DROP TABLE IF EXISTS item_review CASCADE;
+DROP TABLE IF EXISTS bids CASCADE;
+DROP TABLE IF EXISTS items_rounds CASCADE;
 
 CREATE TABLE users (
 	uid			INTEGER,
@@ -22,35 +22,38 @@ CREATE TABLE users (
 	PRIMARY KEY(uid)	
 );
 
+CREATE TABLE tasks (
+	tid 			INTEGER PRIMARY KEY,
+	description 	TEXT
+);
+
 CREATE TABLE admins (
 	uid 		INTEGER		PRIMARY KEY REFERENCES users(uid)
 );
 
-CREATE TABLE rounds (
-	rid 				INTEGER PRIMARY KEY,
-	item_id 			INTEGER REFERENCES items(iid),
-	winning_bid_id		INTEGER UNIQUE REFERENCES rounds(rid),
-	name 				VARCHAR(60),
-	lender_price		INTEGER NOT NULL DEFAULT 0,
-	lender_comments 	TEXT,
-	location			VARCHAR(60),
-	start_date			DATE,
-	end_date			DATE
+CREATE TABLE users_tasks (
+	tid 			INTEGER REFERENCES tasks(tid),
+	uid 			INTEGER REFERENCES users(uid),
+	PRIMARY KEY(tid, uid)
 );
 
 CREATE TABLE items (
-	iid					INTEGER PRIMARY KEY,
-	lender_id 			INTEGER REFERENCES users(uid),
-	current_round		INTEGER UNIQUE REFERENCES rounds(rid),
-	name 				VARCHAR(60),
-	lender_price 		INTEGER NOT NULL DEFAULT 0,
+	iid 				INTEGER PRIMARY KEY,
+	lender_id			INTEGER REFERENCES users(uid),
+	name 				citext,
+	lender_price		INTEGER NOT NULL DEFAULT 0,
 	lender_comments 	TEXT,
-	start_date			DATE,
-	end_date			DATE
+	location			VARCHAR,
+	start_date 			DATE NOT NULL DEFAULT CURRENT_DATE,
+	end_date 			DATE NOT NULL DEFAULT '9999-01-01'	
+);
+
+CREATE TABLE rounds (
+	rid 				INTEGER PRIMARY KEY
 );
 
 CREATE TABLE bids (
-	bid 				INTEGER PRIMARY KEY
+	bid 				INTEGER PRIMARY KEY,
 	round				INTEGER REFERENCES rounds(rid),
 	borrower 			INTEGER REFERENCES users(uid),
 	borrower_price		INTEGER,
@@ -60,13 +63,16 @@ CREATE TABLE bids (
 );
 
 CREATE TABLE user_following (
-	follower_id 		INTEGER PRIMARY KEY REFERENCES users(uid),
-	following_id		INTEGER PRIMARY KEY REFERENCES users(uid),
+	follower_id 		INTEGER,
+	following_id		INTEGER,
+	PRIMARY KEY(follower_id, following_id),
+	FOREIGN KEY(follower_id) REFERENCES users(uid),
+	FOREIGN KEY(following_id) REFERENCES users(uid)
 );
 
-CREATE TABLE item_review (
-	item_rid 		INTEGER PRIMARY KEY,
-	items_id 		INTEGER REFERENCES items(iid),
+CREATE TABLE lender_review (
+	lender_rid 		INTEGER PRIMARY KEY,
+	lid 			INTEGER REFERENCES users(uid),
 	reviewer_id 	INTEGER REFERENCES users(uid),
 	rating			INTEGER,
 	comments 		TEXT,
@@ -82,34 +88,33 @@ CREATE TABLE borrower_review (
 	review_date 	TIMESTAMP
 );
 
-CREATE TABLE lender_review (
-	lender_rid 		INTEGER PRIMARY KEY,
-	lid 			INTEGER REFERENCES users(uid),
+CREATE TABLE item_categories (
+	item_id		INTEGER,
+	uid			INTEGER,
+	PRIMARY KEY(item_id, uid),
+	FOREIGN KEY(item_id) REFERENCES items(iid),
+	FOREIGN KEY(uid) REFERENCES users(uid)
+);
+
+CREATE TABLE wishlist (
+	item_id			INTEGER,
+	uid 			INTEGER,
+	PRIMARY KEY(item_id, uid),
+	FOREIGN KEY(item_id) REFERENCES items(iid),
+	FOREIGN KEY(uid) REFERENCES users(uid)
+);
+
+CREATE TABLE item_review (
+	item_rid 		INTEGER PRIMARY KEY,
+	items_id 		INTEGER REFERENCES items(iid),
 	reviewer_id 	INTEGER REFERENCES users(uid),
 	rating			INTEGER,
 	comments 		TEXT,
 	review_date 	TIMESTAMP
 );
 
-CREATE TABLE wishlist (
-	items_id		INTEGER REFERENCES items(iid),
-	uid 			INTEGER REFERENCES users(uid),
-	PRIMARY KEY(items_id, uid)
-);
-
-CREATE TABLE item_categories (
-	item_id		INTEGER REFERENCES items(iid),
-	uid			INTEGER REFERENCES users(uid),
-	PRIMARY KEY(item_id, uid)
-);
-
-CREATE TABLE tasks (
-	tid 			INTEGER PRIMARY KEY,
-	description 	TEXT
-);
-
-CREATE TABLE users_tasks (
-	tid 			INTEGER REFERENCES tasks(tid),
-	uid 			INTEGER REFERENCES users(uid),
-	PRIMARY KEY(tid, uid)
+CREATE TABLE items_rounds (
+	current_round 	INTEGER UNIQUE REFERENCES rounds(rid),
+	item_id 		INTEGER REFERENCES items(iid),
+	winning_bid_id 	INTEGER UNIQUE REFERENCES bids(bid)
 );
