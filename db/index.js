76754f5760344d513127613fd582774f0caa16fb
jhchia7 <jhchia7@gmail.com>
2019-04-10 02:30:39 +0800

    Add transaction capabilities to db pool

--- db/index.js
+++ db/index.js
@@ -11,5 +11,8 @@ const pool = new Pool({
 module.exports = {
     query: (text, params, callback) => {
         return pool.query(text, params, callback)
+    },
+    connect: (err, client, done) => {
+        return pool.connect(err, client, done)
     }
-}
+};
