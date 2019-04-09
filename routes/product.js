b16de2c2ee5dcc2ccc2405a03bd76ff95e16c6d9
Teo Jia Yun <e0191605@u.nus.edu>
2019-04-09 16:28:55 +0800

    Linked browse result to one item using dummy data

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('product', { title: 'Product' });
+  var data =
+  {
+	title : "Kitchenware",
+  items: {
+          name: "Flower Pot",
+          price: "100",
+          img: "images/k1.jpg"
+        }
+};
+  res.render('product', data);
 });
 
 module.exports = router;
