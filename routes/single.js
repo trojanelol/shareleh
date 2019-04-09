dc84d8fae1d294467aa3673e21c00072c2ed67d1
Teo Jia Yun <e0191605@u.nus.edu>
2019-04-09 15:11:05 +0800

    linked item title to front end

--- routes/single.js
+++ routes/single.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('single', { title: 'Single' });
+  var data =
+  {
+	title : "Flower Pot",
+	price : "100",
+	description :"Great as an example",
+	location : "East",
+	start_date: "04/09/2019",
+	end_date: "04/29/2019"
+};
+  res.render('single', data);
 });
 
 module.exports = router;
