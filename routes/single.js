d61ffafd6b824e895de4d01145ab27bca70874ef
Teo Jia Yun <e0191605@u.nus.edu>
2019-04-10 00:51:15 +0800

    added item reviews

--- routes/single.js
+++ routes/single.js
@@ -4,10 +4,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var data =
   {
-  username: "trojan",
+  username: "trojane",
 	title : "Flower Pot",
   img: "images/si.jpg",
-  rating: "4",
+  rating: "5",
 	price : "100",
 	description :"Great as an example",
 	location : "East",
@@ -20,6 +20,12 @@ router.get('/', function(req, res, next) {
   bidders: [
     {timestamp: "24/4/2019 0000", borrower_username: "butcher1234", bid_price:"120", borrower_rating:"4", description: "for wedding event"},
     {timestamp: "24/4/2019 2359", borrower_username: "butcher55", bid_price:"126", borrower_rating:"5", description: "for science experiment"}
+  ],
+  reviews: [
+    {reviewee: "butcher1234", rating: "5", comment: "cool item"},
+    {reviewee: "damner", rating: "5", comment: "you wish you met this item earlier"},
+    {reviewee: "happyvalley", rating: "5", comment: "good"},
+    {reviewee: "xiaoxuanyeo", rating: "5", comment: "5 STAR WHOOPS"}
   ]
 };
   res.render('single', data);
