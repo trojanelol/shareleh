7a55e687f5a9bc272beb310918ff3818c31e9b7e
bokwoon <bokwoon.c@gmail.com>
2019-04-09 19:04:55 +0800

    reverted names

--- db/init.sql
+++ db/init.sql
@@ -39,10 +39,10 @@ CREATE TABLE admins (
 CREATE TABLE items (
     iid SERIAL,
     current_round INTEGER, -- REFERENCES rounds (rid)
-    item_name citext NOT NULL,
-    lender INTEGER NOT NULL REFERENCES users (uid),
-    lender_price NUMERIC(15,2) NOT NULL DEFAULT 0,
-    lender_comments TEXT,
+    name citext NOT NULL,
+    lid INTEGER NOT NULL REFERENCES users (uid),
+    price NUMERIC(15,2) NOT NULL DEFAULT 0,
+    description TEXT,
     location citext,
     start_date DATE NOT NULL DEFAULT CURRENT_DATE,
     end_date DATE NOT NULL DEFAULT '9999-01-01',
@@ -51,14 +51,14 @@ CREATE TABLE items (
 
 CREATE TABLE rounds (
     rid SERIAL,
-    item INTEGER NOT NULL, -- REFERENCES items (iid)
-    winning_bid INTEGER, -- REFERENCES bids (bid)
+    iid INTEGER NOT NULL, -- REFERENCES items (iid)
+    winning_bid_id INTEGER, -- REFERENCES bids (bid)
     PRIMARY KEY (rid)
 );
 
 CREATE TABLE bids (
     bid SERIAL,
-    round INTEGER, -- REFERENCES rounds (rid)
+    rid INTEGER, -- REFERENCES rounds (rid)
     borrower INTEGER REFERENCES users (uid),
     borrower_price NUMERIC(15,2) NOT NULL DEFAULT 0,
     borrower_comments TEXT,
@@ -68,20 +68,20 @@ CREATE TABLE bids (
 );
 
 ALTER TABLE items ADD CONSTRAINT items_current_round_fkey FOREIGN KEY (current_round) REFERENCES rounds (rid);
-ALTER TABLE rounds ADD CONSTRAINT rounds_item_fkey FOREIGN KEY (item) REFERENCES items (iid);
-ALTER TABLE rounds ADD CONSTRAINT rounds_winning_bid_fkey FOREIGN KEY (winning_bid) REFERENCES bids (bid);
-ALTER TABLE bids ADD CONSTRAINT bids_round_fkey FOREIGN KEY (round) REFERENCES rounds (rid);
+ALTER TABLE rounds ADD CONSTRAINT rounds_iid_fkey FOREIGN KEY (iid) REFERENCES items (iid);
+ALTER TABLE rounds ADD CONSTRAINT rounds_winning_bid_id_fkey FOREIGN KEY (winning_bid_id) REFERENCES bids (bid);
+ALTER TABLE bids ADD CONSTRAINT bids_rid_fkey FOREIGN KEY (rid) REFERENCES rounds (rid);
 
 CREATE TABLE user_following (
-    follower INTEGER REFERENCES users (uid),
-    following INTEGER REFERENCES users (uid),
-    PRIMARY KEY (follower, following)
+    follower_id INTEGER REFERENCES users (uid),
+    following_id INTEGER REFERENCES users (uid),
+    PRIMARY KEY (follower_id, following_id)
 );
 
 CREATE TABLE item_review (
     irid SERIAL,
-    item INTEGER REFERENCES items (iid),
-    reviewer INTEGER REFERENCES users (uid),
+    iid INTEGER REFERENCES items (iid),
+    reviewer_id INTEGER REFERENCES users (uid),
     rating INTEGER NOT NULL,
     comments TEXT,
     review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -90,8 +90,8 @@ CREATE TABLE item_review (
 
 CREATE TABLE borrower_review (
     brid SERIAL,
-    borrower INTEGER REFERENCES users (uid),
-    reviewer  INTEGER REFERENCES users (uid),
+    bid INTEGER REFERENCES users (uid),
+    reviewer_id  INTEGER REFERENCES users (uid),
     rating INTEGER NOT NULL,
     comments TEXT,
     review_date TIMESTAMP,
@@ -100,8 +100,8 @@ CREATE TABLE borrower_review (
 
 CREATE TABLE lender_review (
     lrid SERIAL,
-    lender INTEGER REFERENCES users (uid),
-    reviewer INTEGER REFERENCES users (uid),
+    lid INTEGER REFERENCES users (uid),
+    reviewer_id INTEGER REFERENCES users (uid),
     rating INTEGER NOT NULL,
     comments TEXT,
     review_date TIMESTAMP,
@@ -109,15 +109,15 @@ CREATE TABLE lender_review (
 );
 
 CREATE TABLE wishlist (
-    usr INTEGER REFERENCES users (uid),
-    item INTEGER REFERENCES items (iid),
-    PRIMARY KEY (usr, item)
+    uid INTEGER REFERENCES users (uid),
+    iid INTEGER REFERENCES items (iid),
+    PRIMARY KEY (uid, iid)
 );
 
 CREATE TABLE items_categories (
-    item INTEGER REFERENCES items (iid),
-    category citext,
-    PRIMARY KEY(item, category)
+    iid INTEGER REFERENCES items (iid),
+    cname citext,
+    PRIMARY KEY(iid, cname)
 );
 
 CREATE TABLE tasks (
@@ -128,26 +128,26 @@ CREATE TABLE tasks (
 );
 
 CREATE TABLE users_tasks (
-    usr INTEGER REFERENCES users (uid),
-    task INTEGER REFERENCES tasks (tid),
-    PRIMARY KEY(usr, task)
+    uid INTEGER REFERENCES users (uid),
+    tid INTEGER REFERENCES tasks (tid),
+    PRIMARY KEY(uid, tid)
 );
 
-CREATE OR REPLACE FUNCTION update_changetimestamp_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.changetimestamp = now(); 
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-CREATE TRIGGER update_ab_changetimestamp BEFORE UPDATE
-ON ab FOR EACH ROW EXECUTE PROCEDURE 
-update_changetimestamp_column();
-
-CREATE OR REPLACE FUNCTION trigger_set_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = NOW();
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+-- CREATE OR REPLACE FUNCTION update_changetimestamp_column()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--     NEW.changetimestamp = now(); 
+--     RETURN NEW;
+-- END;
+-- $$ language 'plpgsql';
+-- CREATE TRIGGER update_ab_changetimestamp BEFORE UPDATE
+-- ON ab FOR EACH ROW EXECUTE PROCEDURE 
+-- update_changetimestamp_column();
+--
+-- CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--   NEW.updated_at = NOW();
+--   RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
