637124e1c952c1daab215d64ec4230c5db500812
Teo Jia Yun <e0191605@u.nus.edu>
2019-04-10 00:26:55 +0800

    integrated bidding features on item page for both lender and borrower

--- routes/single.js
+++ routes/single.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var data =
   {
+  username: "trojan",
 	title : "Flower Pot",
   img: "images/si.jpg",
   rating: "4",
@@ -13,7 +14,13 @@ router.get('/', function(req, res, next) {
 	start_date: "04/09/2019",
 	end_date: "04/29/2019",
   lender_username: "trojane",
-  lender_rating: "5"
+  lender_rating: "5",
+  previous_bidding_amount: "120",
+  bidding_status: "Rejected",
+  bidders: [
+    {timestamp: "24/4/2019 0000", borrower_username: "butcher1234", bid_price:"120", borrower_rating:"4", description: "for wedding event"},
+    {timestamp: "24/4/2019 2359", borrower_username: "butcher55", bid_price:"126", borrower_rating:"5", description: "for science experiment"}
+  ]
 };
   res.render('single', data);
 });
