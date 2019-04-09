5989965967ec51c1fdde0182801d39684c742784
Teo Jia Yun <e0191605@u.nus.edu>
2019-04-09 18:01:14 +0800

    Completed front end of user dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Dashboard' });
+  var data =
+  {
+  	title : "Dashboard",
+    user: "butcher1234",
+    tasks: [
+        {task: "Upload one item", progress: "Done"},
+        {task: "Borrow one item", progress: "Done"}
+    ],
+    loan_items: [
+        {item: "Flower Pot", lender: "trojane", return_date: "04/29/2019" },
+        {item: "Party Costumes", lender: "trojane", return_date: "04/29/2019" }
+    ],
+    listings: [
+        {item: "Tupperware", borrower: "trojane", return_date: "04/29/2019" },
+        {item: "Cooking Mama", borrower: "trojane", return_date: "04/29/2019" }
+    ]
+  };
+  res.render('dashboard', data);
 });
 
 module.exports = router;
