a6a8af7e9e5f9798d35fdf972b2799e3ec173196
Teo Jia Yun <e0191605@u.nus.edu>
2019-04-10 19:59:51 +0800

    Protected all pages except index and product

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('checkout', { title: 'Checkout' });
+  res.render('wishlist', { title: 'Wishlist' });
 });
 
 module.exports = router;
