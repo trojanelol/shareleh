1e5134b4c36c780d8217bfbe383dbe6a3d8dbeb4
Teo Jia Yun <e0191605@u.nus.edu>
2019-04-11 11:53:41 +0800

    Linked item end point to index page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ShareLeh Main Page' });
-  // res.send(string/JSON/array)
-  //res.redirect(path)
+
+  browseURL ="http://localhost:3000/api/browse";
+
+  request(browseURL, function (error, response, body) {
+      if (!error) {
+          let data = JSON.parse(body) // Print the google web page.
+          res.render('index', {Title: "Main Page", data: data});
+      }
+
+  })
+
+  // let data = app.get('/api/browse')
+  //
+  // res.render('index', data);
+  // // res.send(string/JSON/array)
+  // //res.redirect(path)
 });
 
 module.exports = router;
